b1e342fc94a037f4dec4d8295e12f5413bc03d75
Max Kerr <rikerr@microsoft.com>
2018-02-06 15:06:51 -0800

    Remove line that adds DefaultCredentials on redirect

--- src/System.Net.Http/src/System/Net/Http/Unix/CurlHandler.EasyRequest.cs
+++ src/System.Net.Http/src/System/Net/Http/Unix/CurlHandler.EasyRequest.cs
@@ -358,10 +358,10 @@ namespace System.Net.Http
                     // Just as with WinHttpHandler, for security reasons, we drop the server credential if it is 
                     // anything other than a CredentialCache. We allow credentials in a CredentialCache since they 
                     // are specifically tied to URIs.
-                    updatedCredentials = _handler._useDefaultCredentials ?
+                    /*updatedCredentials = _handler._useDefaultCredentials ?
                         GetDefaultCredentialAndAuth() : 
-                        GetCredentials(newUri, _handler.Credentials as CredentialCache, s_orderedAuthTypes);
-
+                        GetCredentials(newUri, _handler.Credentials as CredentialCache, s_orderedAuthTypes);*/
+                    updatedCredentials = GetCredentials(newUri, _handler.Credentials as CredentialCache, s_orderedAuthTypes);
                     // Reset proxy - it is possible that the proxy has different credentials for the new URI
                     SetProxyOptions(newUri);
 
@@ -379,6 +379,7 @@ namespace System.Net.Http
                 // Set up the new credentials, either for the new Uri if we were able to get it, 
                 // or to empty creds if we couldn't.
                 SetCredentialsOptions(updatedCredentials);
+                Console.WriteLine(updatedCredentials);
             }
 
             private void SetContentLength(CURLoption lengthOption)
