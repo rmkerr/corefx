f9853c7205afec178746d82b9fba2997021f0eed
Max Kerr <rikerr@microsoft.com>
2018-02-06 13:44:47 -0800

    Remove old rough fix

--- src/System.Net.Http/src/System/Net/Http/Unix/CurlHandler.EasyRequest.cs
+++ src/System.Net.Http/src/System/Net/Http/Unix/CurlHandler.EasyRequest.cs
@@ -362,8 +362,6 @@ namespace System.Net.Http
                         GetDefaultCredentialAndAuth() : 
                         GetCredentials(newUri, _handler.Credentials as CredentialCache, s_orderedAuthTypes);
 
-                    _requestMessage.Headers.Authorization = null;
-
                     // Reset proxy - it is possible that the proxy has different credentials for the new URI
                     SetProxyOptions(newUri);
 
